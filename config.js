323052a520776cebcac051b5bd4e0894bb8bd5db
SamuraiViking <nelson67@stolaf.edu>
2020-05-16 23:17:13 -0500

    undo hardcoding of ssl and connectionString

--- config.js
+++ config.js
@@ -6,10 +6,9 @@ const isProduction = process.env.NODE_ENV === 'production'
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
 const pool = new Pool({
-    // connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-    connectionString: 'postgres://pqmuccjsjynjda:60571e70cb1cae471c6b789850722cac15231ef1e21b1e0b38d9d48cc6300666@ec2-35-171-31-33.compute-1.amazonaws.com:5432/d6vkrof9dlk4ll',
-    // ssl: isProduction,
-    ssl: true,
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    // connectionString: 'postgres://pqmuccjsjynjda:60571e70cb1cae471c6b789850722cac15231ef1e21b1e0b38d9d48cc6300666@ec2-35-171-31-33.compute-1.amazonaws.com:5432/d6vkrof9dlk4ll',
+    ssl: isProduction
 })
 
 module.exports = { pool }
